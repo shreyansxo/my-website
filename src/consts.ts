b0b6cb6a2a0c0837e6192fe9d7920de5b5c14c93
shreyans <shreyansjoshi5@gmail.com>
2025-03-28 15:55:35 +0530

    xo

--- src/consts.ts
+++ src/consts.ts
@@ -3,7 +3,7 @@ import type { TabPanel } from "@components/Skill.astro";
 
 export const SITE: Site = {
   NAME: "Shreyans Joshi",
-  RESUME: "/Resume_Shreyans.pdf",
+  RESUME: "/Resume.pdf",
   NUM_POSTS_ON_HOMEPAGE: 3,
   NUM_WORKS_ON_HOMEPAGE: 2,
   NUM_PROJECTS_ON_HOMEPAGE: 3,
